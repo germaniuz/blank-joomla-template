03c7d2adca96744abf03cd91d52b2403295b77bd
germaniuz <germanius@list.ru>
2017-02-01 23:54:25 +0300

    Add whole strtucture

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width = device-width, initial-scale = 1">
+  <jdoc:include type="head" />
+  <link rel="stylesheet" href="<?php echo  $this->baseurl ?>/templates/system/css/system.css">
+  <link rel="stylesheet" href="<?php echo  $this->baseurl ?>/templates/<?php echo  $this->template ?>/css/base.css">
+</head>
+<body>
+  <h1>I am working!</h1>
+  <script src="<?php echo  $this->baseurl ?>/templates/<?php echo  $this->template ?>/js/functions.js" async></script>
+</body>
+</html>
